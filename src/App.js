3ef674f85eead54111752a315df5f5fbf9bb3bd0
Btaft6278 <btaft6278@gmail.com>
2021-10-13 18:45:56 -0400

    trying again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Avatar from 'react-avatar';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Profile from './components/Profile.js';
 import Leaderboard from './components/Leaderboard.js';
