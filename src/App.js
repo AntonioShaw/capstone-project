847bea82a136f63ea9413a40e5df7bcd22efb11e
David Rouleau <shabbypenguin@gmail.com>
2021-10-13 20:11:33 -0400

    fix merge issues

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import Avatar from 'react-avatar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Profile from './components/Profile.js';
 import Leaderboard from './components/Leaderboard.js';
-import Login from './components/Login.js';
-import Register from './components/Register.js';
+import HomePage from './components/HomePage';
+
 
 function App() {
   return (
-  <Router>
-  <Switch>
-  <Route exact path="/" component={Profile} />
-  <Route exact path="/login" component={Login} />
-  <Route exact path="/register" component={Register} />
-  {/* <Route exact path="/profile" component={Profile} /> */}
-  <Route exact path="/leaderboards" component={Leaderboard} />
-  </Switch>
-  </Router>
+    <Router>
+    <Switch>
+    <Route exact path="/" component={HomePage} />
+    <Route exact path="/login" component={login} />
+    <Route exact path="/register" component={register} />
+    <Route exact path="/profile" component={Profile} />
+    <Route exact path="/leaderboards" component={Leaderboard} />
+    </Switch>
+    </Router>
   )
 }
 
 
 
 export default App;
-  
