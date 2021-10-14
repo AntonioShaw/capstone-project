b731749ea0844a7f6dcfaff5f9c58d289cedf022
David Rouleau <shabbypenguin@gmail.com>
2021-10-13 20:08:50 -0400

    try to fix merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-// import Avatar from 'react-avatar';
+import Avatar from 'react-avatar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Profile from './components/Profile.js';
 import Leaderboard from './components/Leaderboard.js';
 import HomePage from './components/HomePage';
 
+
 function App() {
   return (
-  <HomePage /> 
+    <Router>
+    <Switch>
+    <Route exact path="/" component={HomePage} />
+    <Route exact path="/login" component={login} />
+    <Route exact path="/register" component={register} />
+    <Route exact path="/profile" component={Profile} />
+    <Route exact path="/leaderboards" component={Leaderboard} />
+    </Switch>
+    </Router>
   )
 }
 
