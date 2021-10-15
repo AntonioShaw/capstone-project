7642b232653079a9169a42ae63f8d9ea5cb35d12
AntonioShaw <tonyshaw0613@gmail.com>
2021-10-15 11:51:02 -0400

    Moved router, fixed background image, fixed homepage button placement.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -14,43 +14,60 @@ import NavBar from './NavBar';
 
 
 function HomePage() {
+    const styling = {
+        backgroundColor: 'rgba(255, 255, 255, .15)',
+        backdropFilter: 'blur(5px)',
+        color: 'black',
+        flexDirection: 'column',
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        textAlign: 'left',
+        opacity: '.85',
+        border: '2px solid black',
+        width: '35%',
+        padding: '10px',
+        minWidth: '325px'
+    }
     return (
         <div className="HomePage">
             <NavBar />
             <div className="container">
                 <Row>
-                    <Col><button className="ProfileButton">Question Count</button></Col>    
+                    <Col><button className="ProfileButton">Question Count</button></Col>
                     <Col><button className="ProfileButton">Your Ranking</button></Col>
                 </Row>
-                
-                <Row className="HomeBlurb">
+
+                <Row className="HomeBlurb" style={styling}>
                     <p className="Instructions">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a type specimen book. 
+                        Welcome to QuizWiz, a fun trivia game where your knowledge of various topics 
+                        can be put to the test. You can select from any one of twelve different trivia
+                        categories ranging from topics across pop culture, entertainment, and academia.
+                        <br/>
+                        <br/>
+                        Rack up as many correct answers as you can within the time limit to place on
+                        the leaderboard and prove to everyone that you are the one, true QuizWiz!
                     </p>
                 </Row>
-
+                <div style={styling}>
                 <Row>
-                    <Container>
-                        <ButtonGroup className="ButtonGroup"  vertical>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">General Information</Button></Link>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">Books</Button></Link>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">Movies</Button></Link>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">TV</Button></Link>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">Music</Button></Link>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">Video Games</Button></Link>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">Math</Button></Link>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">Computers</Button></Link>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">Geography</Button></Link>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">Sports</Button></Link>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">Celebrities</Button></Link>
-                            <Link className="QuizLink" to={{pathname: `/quiz`, query:{category: '9'}}}><Button className="QuizButton">Random</Button></Link>
-                        </ButtonGroup>
-                    </Container>     
+                    <ButtonGroup className="ButtonGroup" vertical block style={{display: 'flex', alignItems: 'center', justifyContent: 'center',maxWidth: '100%'}}> 
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">General Information</Button></Link>
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">Books</Button></Link>
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">Movies</Button></Link>
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">TV</Button></Link>
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">Music</Button></Link>
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">Video Games</Button></Link>
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">Math</Button></Link>
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">Computers</Button></Link>
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">Geography</Button></Link>
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">Sports</Button></Link>
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">Celebrities</Button></Link>
+                        <Link className="QuizLink" to={{ pathname: `/quiz`, query: { category: '9' } }}><Button className="QuizButton">Random</Button></Link>
+                    </ButtonGroup>
                 </Row>
-            </div>   
+                </div>
+            </div>
         </div>
     )
 }
