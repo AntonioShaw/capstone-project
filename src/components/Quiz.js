be5a333d547f2088f714519b5e51e69e0b44e4b5
David Rouleau <shabbypenguin@gmail.com>
2021-10-18 15:02:31 -0400

    broken mess of code

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -10,49 +10,65 @@ function StartQuiz(props) {
 	let [questioncounter, setquestioncounter] = useState(0)
 	const [questions, setquestions] = useState()
 	const [correctanswer, setcorrectanswer] = useState(null)
+	const [answers, setanswers] = useState([])
 	let [quizfinished, setquizfinished] = useState(false)
-	let score=0
+	let currentanswers = []
+	let [score, setcorrectquestions] = useState([])
 	
 	function getquestions(props) {
-		console.log(props)
 		fetch(`https://quizwiz.glitch.me/quiz?category=9`)
 		.then(response => response.json())
 		.then(result => {
-			console.log(result)
 			setquestions([...result])
-			console.log(questions)
 			playquiz()
 		})
 		.catch(error => console.log('error', error))
 	}
 	
-	
-	
-	function playquiz() {
-		let currentanswers = []
-		setcurrentquestion((questions[questioncounter].question).replace(/(&quot\;)/g,"\"").replace(/(&shy\;)/g,"-").replace(/(&#039\;)/g,"'").replace(/(&oacute\;)/g,"ó").replace(/(&rsquo\;)/g,"’").replace(/(&ldquo\;)/g,"“"))
-		setcorrectanswer((questions[questioncounter].correct_answer).replace(/(&quot\;)/g,"\"").replace(/(&shy\;)/g,"-").replace(/(&#039\;)/g,"'").replace(/(&oacute\;)/g,"ó").replace(/(&rsquo\;)/g,"’").replace(/(&ldquo\;)/g,"“"))
-		
-
-		for(let i=0; i<3; i++){
-			currentanswers.push((questions[questioncounter].incorrect_answers[i]).replace(/(&quot\;)/g,"\"").replace(/(&shy\;)/g,"-").replace(/(&#039\;)/g,"'").replace(/(&oacute\;)/g,"ó").replace(/(&rsquo\;)/g,"’").replace(/(&ldquo\;)/g,"“"))
-		}
-		currentanswers.push(questions[questioncounter].correct_answer)
-		console.log(currentanswers.sort())
-		console.log(questioncounter)
-		
-		if(questioncounter == 9){
-			console.log("should be finished!")
+	function gamesOver() {
+		if(questioncounter == 10){
+			console.log("Your score is: ")
+			console.log(score.length)
 			//setquizfinished(true)
 		}
-		setquestioncounter(questioncounter+=1)
+	}
 
+	
+	function playquiz() {
+		if(questioncounter == 10){
+			gamesOver()
+		}
+		currentanswers = []
+		setcurrentquestion((questions[questioncounter].question).replace(/(&quot;)/g,"\"").replace(/(&shy;)/g,"-").replace(/(&#039;)/g,"'").replace(/(&oacute;)/g,"ó").replace(/(&rsquo;)/g,"’").replace(/(&ldquo;)/g,"“").replace(/(&hellip;)/g,"...").replace(/(&rdquo;)/g, "\""))
+		setcorrectanswer((questions[questioncounter].correct_answer).replace(/(&quot;)/g,"\"").replace(/(&shy;)/g,"-").replace(/(&#039;)/g,"'").replace(/(&oacute;)/g,"ó").replace(/(&rsquo;)/g,"’").replace(/(&ldquo;)/g,"“").replace(/(&hellip;)/g,"...").replace(/(&rdquo;)/g, "\""))
+		
+		for(let i=0; i<3; i++){
+			currentanswers.push((questions[questioncounter].incorrect_answers[i]).replace(/(&quot;)/g,"\"").replace(/(&shy;)/g,"-").replace(/(&#039;)/g,"'").replace(/(&oacute;)/g,"ó").replace(/(&rsquo;)/g,"’").replace(/(&ldquo;)/g,"“").replace(/(&rdquo;)/g, "\"").replace(/(&hellip;)/g, "..."))
+		}
+		currentanswers.push((questions[questioncounter].correct_answer).replace(/(&quot;)/g,"\"").replace(/(&shy;)/g,"-").replace(/(&#039;)/g,"'").replace(/(&oacute;)/g,"ó").replace(/(&rsquo;)/g,"’").replace(/(&ldquo;)/g,"“").replace(/(&rdquo;)/g, "\"").replace(/(&hellip;)/g, "..."))
+		setanswers(currentanswers.sort(() => Math.random() - 0.5))
 
 		
+
+		setquestioncounter(questioncounter+=1)	
+	}
+	
+	function checkAnswer(i) {
+		if (answers[i] == correctanswer){
+			console.log("right answer!")
+			setcorrectquestions(score => [...score, "correct"])
+			console.log(score.length)
+			playquiz()
+		}else{
+			console.log("you dummy")
+			console.log(score.length)
+			playquiz()
+		}
 	}
 
 	
 	useEffect(() => {
+		console.log(score)
 			getquestions()
 			
 		return (
@@ -65,7 +81,12 @@ function StartQuiz(props) {
 	return (
 		<div>
 		{currentquestion}
-		{questions == null ? <Button onClick={() => playquiz()}>Press to start</Button> : <Button onClick={() => playquiz()}>Next question</Button>	}
+		<Button onClick={() => playquiz()}>Next question</Button>
+		<br /><br /><br />
+		<li><Button onClick={() => checkAnswer(0)}>{answers[0]}</Button></li>
+		<li><Button onClick={() => checkAnswer(1)}>{answers[1]}</Button></li>
+		<li><Button onClick={() => checkAnswer(2)}>{answers[2]}</Button></li>
+		<li><Button onClick={() => checkAnswer(3)}>{answers[3]}</Button></li>
 		</div>
 	)
 }
