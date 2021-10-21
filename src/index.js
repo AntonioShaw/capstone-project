59d55758445d66e26b36ed5cf55d9f69bf30f513
David Rouleau <shabbypenguin@gmail.com>
2021-10-21 11:10:03 -0400

    add scroll on every page load

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './components/scrolltop.js';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+  <ScrollToTop />
+
     <App />
     </Router>
   </React.StrictMode>,
