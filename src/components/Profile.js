1ba96c90ba0f1bcd3ddbae668a39d7dd7d6cb975
Btaft6278 <btaft6278@gmail.com>
2021-10-22 10:40:10 -0400

    friday morning profile and leaderboard

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,8 +7,54 @@ import  Avatar from "react-avatar";
 import  NavBar from "./NavBar.js";
 import Leaderboard from "./Leaderboard.js";
 
+
+
 function Profile(props) {
 	
+//     const [myScore, setMyScore] = useState([])
+
+  
+//  useEffect(() => { getMyScore() }, [])
+//  const name = localStorage.getItem('name')
+//   const getMyScore = () => {
+    
+//    const username = name
+//     console.log(name)
+//     fetch(`https://quiz-wiz-server.glitch.me/api/userscore/${username}`)
+//       .then(response => response.json())
+//       .then(myScore => {
+//         console.log(myScore)
+
+//         setMyScore(myScore)
+//       })
+//   }
+
+  const [leaderboard, setLeaderboard] = useState([])
+
+  
+  useEffect(() => { getLeaderboard() }, [])
+
+  const getLeaderboard = (props) => {
+    fetch('https://quiz-wiz-server.glitch.me/api/highscore')
+      .then(response => response.json())
+      .then(result => {
+        console.log(result[0])
+        setLeaderboard(result)
+        console.log(leaderboard)
+      }
+      )
+  }
+  
+  const leaderboardList = leaderboard.map((score,index) => {
+      return(
+      <ul key={index} >
+      <h1>Wizard:{score.username}</h1>
+      <h3>High Score:{score.score}</h3>
+      </ul>
+      )
+
+})
+
 	
 	return(
 		<div id=" profile-page">
@@ -18,21 +64,26 @@ function Profile(props) {
         <div id = 'bottom-container'>
             <div id="profile-container">
                 <div id=" avatar-container">
-                    <Avatar color={Avatar.getRandomColor('sitebase', ['red', 'green', 'blue'])} size="150"/>
-                    
-
+                <a  href="https://quizwiz.me">
+                <a href="https://quizwiz.me">
+                    <img id="avatar" src="/1myavatar.jpg" alt=""  style={{ backgroundColor: "blue" }}></img>
+                </a>
+                
+                </a>
                 </div>                    
                 <div id='info-container'>
-                    USERNAME or YADDA YADDA YADDA
+                   Hello "Wizard"<br></br>
+                   Your current high score is "myScore"
                 </div>
+                <Link to={'/logout'}><Button variant="primary">Log Out</Button></Link>
+		<Link to={'/deleteprofile'}><Button variant="danger">Delete Account</Button></Link>
             </div>
-            <div id="leaderboard-container">
-                <Leaderboard />
+            <div id="top-ten">
+              {leaderboardList}
             </div>    
         </div>
-		Your current high score is <UserHighScore />
-		<Link to={'/logout'}><Button variant="primary">Log Out</Button></Link>
-		<Link to={'/deleteprofile'}><Button variant="danger">Delete Account</Button></Link>
+		
+		
 		</div>
 
         
