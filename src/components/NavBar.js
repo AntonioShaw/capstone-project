de578ce81d267bc8aaa8328bc0b0645bdfce6c73
Antonio Shaw <tonyshaw0613@gmail.com>
2021-10-23 14:28:21 -0400

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -32,7 +32,7 @@ function Navbar() {
                     <NavLink to="leaderboard" className='inactive' id='a' >Leaderboard</NavLink>
 		    <NavLink to={'profile/'+localStorage.getItem('wizardName')} className='inactive' id='a' >Profile</NavLink>
                 </ul>
-                <img src="/QuizWizNav2.png" alt="QuizWiz Logotype"/>
+                <img id="logotype" src="/QuizWizNav2.png" alt="QuizWiz Logotype"/>
             </div>
         </nav>
     )
